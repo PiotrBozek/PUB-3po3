51c063365579eace69ca0024b9e965a6a11042e5
Piotr <boziki@gazeta.pl>
2020-11-26 14:08:48 +0100

    Upgrade 1

--- src/components/DivRight.js
+++ src/components/DivRight.js
@@ -4,6 +4,7 @@ import React from 'react';
 export default () => {
     return (
         <div className='div-right'>
+            <p class="open">Czynne pon.- pt. i niedz. 12-22, sob. 12-23 </p>
             <p>Dowóz na terenie Żagania gratis</p>
         </div>
     )
